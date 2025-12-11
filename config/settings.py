e3e7faa568a550d625cd896bbf416a7fbba8a3b6
Gaëtan Faure-Levoux <gaetan@faure-levoux.fr>
2025-12-11 15:46:04 +0100

    test Ci process 15

--- config/settings.py
+++ config/settings.py
@@ -3,7 +3,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     SMTP_HOST: str
-    SMTP_PORT: str
+    SMTP_PORT: int
     SMTP_USER: str
     SMTP_PASSWORD: str
     model_config = SettingsConfigDict()
