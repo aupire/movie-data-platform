7de1afbfcd3c8f8c91ebd8bedcaaa2e528abe143
Gaëtan levoux <gaetan@faure-levoux.fr>
2025-12-12 15:46:54 +0100

    send to github

--- config/settings.py
+++ config/settings.py
@@ -6,13 +6,13 @@ class Settings(BaseSettings):
     SMTP_PORT: int
     SMTP_USER: str
     SMTP_PASSWORD: str
-    
+
     DB_NAME: str
     DB_USER: str
     DB_PASSWORD: str
     DB_HOST: str
     DB_PORT: int
-    
+
     model_config = SettingsConfigDict()
 
 
