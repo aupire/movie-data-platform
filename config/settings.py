9690f7ff42f2c17e83ed1937200e6d43f6d45a7e
Gaëtan levoux <gaetan@faure-levoux.fr>
2025-12-13 16:50:20 +0100

    first lauch instance test 5

--- config/settings.py
+++ config/settings.py
@@ -3,18 +3,18 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
 
-    # -----------------------------
+    # ------------------------------
     # Email (SMTP)
-    # -----------------------------
+    # ------------------------------
     SMTP_HOST: str
     SMTP_PORT: int
     SMTP_USER: str
     SMTP_PASSWORD: str
     OWNER_EMAIL: str
 
-    # -----------------------------
+    # ------------------------------
     # Database
-    # -----------------------------
+    # ------------------------------
     DB_NAME: str
     DB_USER: str
     DB_PASSWORD: str
