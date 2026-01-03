0b3475fe36c5685a650b6c797789201bba5396bb
Gaëtan levoux <gaetan@faure-levoux.fr>
2026-01-03 20:47:06 +0100

    simplification of the app 3

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
-from utils import email
-import time
+from flask import Flask, request, jsonify
+from waitress import serve
+import os
 
-for i in range(5):
-    email.send()
-    time.sleep(1)
+
+HOST = os.getenv("HOST", "127.0.0.1")
+PORT = int(os.getenv("PORT", "8080"))
+
+app = Flask(__name__)
+
+
+@app.route("/", methods=["GET"])
+def echo_get_params():
+    """
+    Renvoie tous les paramètres GET reçus sous forme JSON.
+    """
+    params = request.args.to_dict(flat=True)
+    return jsonify(params), 200
+
+
+if __name__ == "__main__":
+    serve(app, host=HOST, port=PORT)
